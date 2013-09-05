f0bd80f6698800052118c4f741898e4c3c5b8351
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2013-09-05 09:55:47 +0000

    version 0.90.2

--- R/likelihood.R
+++ R/likelihood.R
@@ -14,8 +14,9 @@
 #'   \item{icl}{the estimated ICL criterion.}
 #' @examples
 #' data(big4)
-#' res=rankclust(big4$data,m=big4$m,K=2,Ql=100,Bl=50,maxTry=2)
-#' crit=criteria(big4$data,res[2]@@proportion,res[2]@@pi,res[2]@@mu,big4$m,Ql=200,Bl=100)
+#' res=rankclust(big4$data,m=big4$m,K=2,Ql=100,Bl=50,maxTry=3)
+#' if(res@@convergence==TRUE)
+#' 	crit=criteria(big4$data,res[2]@@proportion,res[2]@@pi,res[2]@@mu,big4$m,Ql=200,Bl=100)
 #' @export
 criteria <-function(data,proportion,pi,mu,m,Ql=500,Bl=100)
 {
